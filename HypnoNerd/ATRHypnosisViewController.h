1d1c6c94a167524b50d5c0408949a7e3d1750ef3
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-20 12:04:04 +0530

    Create the ui view controller for hypnosis

---
+++ HypnoNerd/ATRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Leonard Bogdonoff on 10/20/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATRHypnosisViewController : UIViewController
+
+@end
